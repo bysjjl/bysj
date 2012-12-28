0b2b54fe7bf8133c12fba5fefb5835bcc626446a
bysjjl <2631053041@qq.com>
2012-12-28 16:51:51 +0800

    完善Player类

--- bysjjl/src/org/primefaces/examples/domain/Player.java
+++ bysjjl/src/org/primefaces/examples/domain/Player.java
@@ -1,5 +1,13 @@
 package org.primefaces.examples.domain;
 
 public class Player {
-
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Player(String name, int age, String url) {
+	}
 }
